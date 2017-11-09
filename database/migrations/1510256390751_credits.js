8aa5bfa12af5ba89fd71a3de6112f9ac1d4a73ec
Franco Correa <anibalfranciscocorrea@gmail.com>
2017-11-09 16:45:14 -0300

    Create migration for credits customer

---
+++ database/migrations/1510256390751_credits.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class CreditsTableSchema extends Schema {
+
+  up () {
+    this.create('credits', (table) => {
+      table.increments()
+      table.decimal('amount')
+      table.integer('customer_id').unsigned().references('id').inTable('customers')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('credits')
+  }
+
+}
+
+module.exports = CreditsTableSchema
