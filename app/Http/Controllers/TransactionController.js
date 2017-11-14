b2b0ee803bac66085906f5c90f6e3124a81afd8b
Franco Correa <anibalfranciscocorrea@gmail.com>
2017-11-14 13:08:34 -0300

    get all transactions lists

---
+++ app/Http/Controllers/TransactionController.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const Transaction = use('App/Model/Transaction')
+
+class TransactionController {
+
+    /**
+     * List all transactions
+     * 
+     * @param {*} request 
+     * @param {*} response 
+     */
+    * index (request, response) {
+        const transactions = yield Transaction.query()
+        .with('customer', 'service')
+        .fetch()
+
+        yield response.status(200).json({error: false, transactions: transactions})
+    }
+
+    /**
+     * Get transactions by customer id.
+     * 
+     * @param {*} request 
+     * @param {*} response 
+     */
+    * show (request, response) {
+        const transactions =  yield Transaction
+        .with('customer', 'service')
+        .where('customer_id', request.param('id'))
+        .fetch()
+        
+        yield response.status(200).json({error: false, transactions: transactions})
+    }
+
+}
+
+module.exports = TransactionController
