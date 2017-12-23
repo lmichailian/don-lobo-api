85ca5df99ec1897e29f963361a8a487e367f2363
Lucas Michailian <lucasmichailian@gmail.com>
2017-12-22 21:56:45 -0300

    minor changes

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -23,25 +23,24 @@ Route.post('/login', 'UserController.login')
  * Routing api donlobo v1
  */
 Route.group('version1', function () {
-    //User routes
-
-    Route.get('/me', 'UserController.user')
+    // User routes
+  Route.get('/me', 'UserController.user')
     // Customers routes
-    Route.get('/customers', 'CustomerController.index')
-    Route.post('/customers', 'CustomerController.store')
-    Route.put('/customers/:id', 'CustomerController.update')
-    Route.delete('/customers/:id', 'CustomerController.delete')
+  Route.get('/customers', 'CustomerController.index')
+  Route.post('/customers', 'CustomerController.store')
+  Route.put('/customers/:id', 'CustomerController.update')
+  Route.delete('/customers/:id', 'CustomerController.delete')
 
     // Credit routes
-    Route.post('/credits', 'CreditController.store')
-    Route.get('/credits/customers/:card', 'CreditController.show')
+  Route.post('/credits', 'CreditController.store')
+  Route.get('/credits/customers/:card', 'CreditController.show')
 
     // Charge Service routes
-    Route.post('/services/charges', 'ServiceController.store')
+  Route.post('/services/charges', 'ServiceController.store')
 
     // Transaction routes
-    Route.get('/transactions', 'TransactionController.index')
-    Route.get('/transactions/customers/:id', 'TransactionController.show')
+  Route.get('/transactions', 'TransactionController.index')
+  Route.get('/transactions/customers/:id', 'TransactionController.show')
 })
     .prefix('/api/v1')
     .middleware('authApi')
